bb3afa59069d48a808eec64344c0f33a67896397
germanquevedo <mgerman.quevedo@gmail.com>
2024-08-27 23:32:20 -0300

    agrego funcion para desencriptar

--- index.js
+++ index.js
@@ -18,9 +18,31 @@ function encriptar(){
 
     } else{
         muñeco.src = "./imagenes/Muñeco.png";
-        parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
         alert("Debes ingresar algún texto");
     }
-
 }
+ function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+    } else{
+        muñeco.src = "./imagenes/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+ }   
